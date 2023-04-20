c269fa3147d053c677e70036384746c8b2ec455c
DariusGracey <Dariusgracey814@gmail.com>
2023-04-19 22:44:22 -0400

    updated file extensions to react

--- src/global.js
+++ src/global.js
@@ -1,17 +1,14 @@
 "use strict";
 
 export function scrollToSection(evt) {
-    const link = evt.target;
-    const href = link.getAttribute('href');
+  const link = evt.target;
+  const href = link.getAttribute("href");
 
-    console.log(href);
-
-    if(href !== "#" && href.startsWith('#')) {
-        // Target Scroll Section
-        const targetSection = document.querySelector(href);
-        targetSection.scrollIntoView({behavior: "smooth"});
-    }
-    else {
-        return;
-    }
+  if (href !== "#" && href.startsWith("#")) {
+    // Target Scroll Section
+    const targetSection = document.querySelector(href);
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  } else {
+    return;
+  }
 }
